87e66816df0aa906f72f26aeb884ebdc139e387c
sanjiv-acharya07 <112199174+sanjiv-acharya07@users.noreply.github.com>
2024-07-09 16:04:32 -0400

    Create IllegalPurchaseException.java

---
+++ IllegalPurchaseException.java
@@ -0,0 +1,9 @@
+public class IllegalPurchaseException extends Exception {
+    public IllegalPurchaseException (String message) {
+        super(message);
+    }
+
+    public IllegalPurchaseException() {
+        super();
+    }
+}
